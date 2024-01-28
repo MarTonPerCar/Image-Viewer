d9523306cd88cbf01ee5a7ebf50799efa3dc6038
MarTonPerCar <mario2002pc@hotmail.com>
2024-01-28 02:29:58 +0000

    agregarBotones method

--- src/main/java/ImageViewer/Image.java
+++ src/main/java/ImageViewer/Image.java
@@ -1,6 +1,7 @@
 package ImageViewer;
 
 public interface Image {
+    public static int ActualValue = 0;
     void next();
     void prev();
 }
--- src/main/java/ImageViewer/Swing/MainFrame.java
+++ src/main/java/ImageViewer/Swing/MainFrame.java
@@ -10,18 +10,38 @@ import java.util.List;
 public class MainFrame extends JFrame {
     public MainFrame(List<BaseImage> iP)  {
         this.setTitle("Image Viewer");
-        this.setSize(600,700);
+        this.setSize(520,400);
         this.setLocationRelativeTo(null);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+
         JPanel panel = new JPanel(new BorderLayout());
-        panel.setBorder(new EmptyBorder(20, 0, 0, 0));
+        panel.setBackground(Color.lightGray);
+        panel.setBorder(new EmptyBorder(10, 0, 0, 0));
+
         nuevoLabel(iP, 0, panel);
+        agregarBotones(panel);
+
+        add(panel);
     }
 
     private void nuevoLabel(List<BaseImage> iP, int imagen, JPanel panel) {
         ImageIcon imageIcon = new ImageIcon(iP.get(imagen).path());
         JLabel imageLabel = new JLabel(imageIcon);
         panel.add(imageLabel, BorderLayout.NORTH);
-        add(panel);
+    }
+
+    private void agregarBotones(JPanel panel) {
+        JButton botonIzquierdo = new JButton("Prev");
+        JButton botonDerecho = new JButton("Next");
+
+        botonIzquierdo.setPreferredSize(new Dimension(210, 30));
+        botonDerecho.setPreferredSize(new Dimension(210, 30));
+
+        JPanel panelBotones = new JPanel(new BorderLayout());
+        panelBotones.setBackground(Color.lightGray);
+
+        panelBotones.add(botonIzquierdo, BorderLayout.WEST);
+        panelBotones.add(botonDerecho, BorderLayout.EAST);
+        panel.add(panelBotones, BorderLayout.SOUTH);
     }
 }
